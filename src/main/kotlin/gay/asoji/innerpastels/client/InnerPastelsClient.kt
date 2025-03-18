14e7ddbe80193bf6e162847cd24a621e854547c1
Naz <bluspringindohq@gmail.com>
2025-03-18 22:00:37 +0800

    Fix capes not getting initialized

--- src/main/kotlin/gay/asoji/innerpastels/client/InnerPastelsClient.kt
+++ src/main/kotlin/gay/asoji/innerpastels/client/InnerPastelsClient.kt
@@ -1,14 +1,16 @@
 package gay.asoji.innerpastels.client
 
+import gay.asoji.innerpastels.capes.CapeUtils
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.loader.api.FabricLoader
 
 class InnerPastelsClient : ClientModInitializer {
     override fun onInitializeClient() {
         if (FabricLoader.getInstance().isDevelopmentEnvironment) {
-
             ImGuiClient.init()
         }
+
+        CapeUtils.INSTANCE.init()
     }
 
 
